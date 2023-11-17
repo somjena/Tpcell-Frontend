a156c5b1c4868babb8ab461676190d33682ca787
Soumitra jena <131749408+somjena@users.noreply.github.com>
2023-11-17 17:37:30 +0530

    Updated Home
    Done

--- frontend/src/Component/Home.jsx
+++ frontend/src/Component/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import { Button } from 'react-bootstrap'; // Import Button component from Bootstrap
-
-const Home = () => {
+function HomePage(){
     return (
-        <div>
-            {/* Your other components or content */}
-            <Button variant="primary">Login</Button> {/* Bootstrap Login button */}
+        <div className="home">
+            <div className="jumbotron bg-transparent p-4 text-center border-bottom">
+                <h4>Welcome to Centurion Placement System</h4>    
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4 mx-auto mt-5 text-center shadow p-2"  onClick={()=>window.location.href='/login'}>
+                        <img src={'images/Login.png'} className="img-thumbnail" style={{height:"150px",width:"150px"}} />
+                        <h5 className="p-2 text-white">LOGIN</h5>
+                    </div>                                                    
+                </div>
+            </div>
         </div>
-    );
-};
-
-export default Home;
+        )
+    }
+export default HomePage;
