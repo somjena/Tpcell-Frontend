f09f9516ce724323285d94c2ed223eb3e74ea5af
Soumitra jena <131749408+somjena@users.noreply.github.com>
2023-11-17 17:28:24 +0530

    Created Home
    created the home component and render

---
+++ frontend/src/Component/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { Button } from 'react-bootstrap'; // Import Button component from Bootstrap
+
+const Home = () => {
+    return (
+        <div>
+            {/* Your other components or content */}
+            <Button variant="primary">Login</Button> {/* Bootstrap Login button */}
+        </div>
+    );
+};
+
+export default Home;
